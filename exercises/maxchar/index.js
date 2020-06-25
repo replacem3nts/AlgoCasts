184b3fb5e7219aa8b219a39f68cc1d1ec5102e46
replacem3nts <donny.landis@gmail.com>
2020-06-25 15:41:02 -0400

    solves maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let max = 0
+    let maxChar = ''
+    let charHash = {}
+
+    for (let char of str) {charHash[char] ? charHash[char]++ : charHash[char] = 1}
+
+    for (let char in charHash) {
+        if(charHash[char] > max) {
+            max = charHash[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
 
 module.exports = maxChar;
