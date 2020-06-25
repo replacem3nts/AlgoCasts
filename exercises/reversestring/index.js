57094750d04a0de91cbe6828e8fd42b1e0ec9432
replacem3nts <donny.landis@gmail.com>
2020-06-25 13:31:59 -0400

    solves reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,22 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     let rev = ''
+
+//     for(let char of str) {
+//         rev = char + rev
+//     }
+
+//     return rev
+// }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
