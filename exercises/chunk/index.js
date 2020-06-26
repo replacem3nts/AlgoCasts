aa066fc19b963d6fe0146afd15f6d33a9342597b
replacem3nts <donny.landis@gmail.com>
2020-06-26 13:49:40 -0400

    solves chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,44 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // HIS ALT SOLVE (SIMILAR TO MINE, BUT NICER)
+    let chunkedArr = []
+    let index = 0
+
+    while (index < array.length) {
+        chunkedArr.push(array.slice(index, index + size))
+        index += size
+    }
+
+    return chunkedArr
+}
 
 module.exports = chunk;
+
+// HIS V1 SOLVE
+// function chunk(array, size) {
+//     let chunkedArr = []
+//     for (let element of array) {
+//         let last = chunkedArr[chunkedArr.length - 1]
+//         if (!last || last.length === size) {
+//             chunkedArr.push([element])
+//         } else {
+//             last.push(element)
+//         }
+//     }
+//     return chunkedArr
+// }
+
+//MY INITIAL SOLVE
+// function chunk(array, size) {
+//     let chunkedArrArr = []
+//     let lastIndx = 0
+//     for(let i = 0; i < array.length; i++) {
+//         if ((i + 1) % size === 0 || i === array.length - 1) {
+//             chunkedArrArr.push(array.slice(lastIndx, i + 1))
+//             lastIndx = i + 1
+//         }
+//     }
+//     return chunkedArrArr
+// }
