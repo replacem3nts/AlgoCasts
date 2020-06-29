6d7455fc8abe6198ac8f22c9c36761bf58308de4
replacem3nts <donny.landis@gmail.com>
2020-06-29 19:47:57 -0400

    solves pyramid and vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,103 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// 00 01 02 03 04
+// 10 11 12 13 14
+// 20 21 22 23 24
+
+function pyramid(n) {
+    let midpoint = Math.floor((2 * n - 1) / 2)
+
+    for(let row = 0; row < n; row++){
+        let level = ''
+
+        for(let column = 0; column < 2 * n - 1; column++) {
+            if(midpoint - row <= column && midpoint + row >= column) {
+                level += '#'
+            } else {
+                level += ' '
+            }
+        }
+        console.log(level)
+    }
+}
+
 
 module.exports = pyramid;
+
+// His iterative solve
+// function pyramid(n) {
+//     let midpoint = Math.floor((2 * n - 1) / 2)
+
+//     for(let row = 0; row < n; row++){
+//         let level = ''
+
+//         for(let column = 0; column < 2 * n - 1; column++) {
+//             if(midpoint - row <= column && midpoint + row >= column) {
+//                 level += '#'
+//             } else {
+//                 level += ' '
+//             }
+//         }
+//         console.log(level)
+//     }
+// }
+
+// MY FAVORITE SOLVE
+// function pyramid(n, row = 0) {
+//     if(n === row) {
+//         return
+//     }
+
+//     let spaceNo = n - row - 1
+//     let hashNo = row * 2 + 1
+
+//     let spaceArray = Array(spaceNo).fill(' ')
+//     let hashArray = Array(hashNo).fill('#')
+
+//     let stepArray = [...spaceArray, ...hashArray, ...spaceArray]
+//     let step = stepArray.join('')
+//     console.log(step)
+
+//     pyramid(n, row += 1)
+// }
+// MY RECURSIVE SOLVE
+// function pyramid(n, row = 0, step = '') {
+//     if(n === row) {
+//         return
+//     }
+//     let rowLength = n * 2 - 1
+//     let spaceNo = n - row - 1
+
+//     if(step.length === rowLength) {
+//         console.log(step)
+//         pyramid(n, row += 1)
+//         return
+//     }
+
+//     if(step.length < rowLength) {
+//         step.length < spaceNo || step.length >= (rowLength - spaceNo) ? step += ' ' : step += '#'
+//         pyramid(n, row, step)
+//     }
+// }
+
+// MY ITERATIVE SOLVE
+// function pyramid(n) {
+//     for(let row = 0; row < n; row++) {
+
+//         let hashNo = row * 2 + 1
+//         let hashes = elementBuilder('#', hashNo)
+
+//         let spaceNo = n - row - 1
+//         let spaces = elementBuilder(' ', spaceNo)
+
+//         let step = spaces + hashes + spaces
+//         console.log(step)
+//     }
+// }
+
+// function elementBuilder(element, number) {
+//     let result = ''
+//     for(let y = 0; y < number; y++) {result += element}
+//     return result
+// }
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,35 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let matches = str.match(/[aeiou]/gi)
+    return !!matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+// His iterative solve
+// function vowels(str) {
+//     let count = 0
+//     let vowelsArr = ['a', 'e', 'i', 'o', 'u']
+
+//     for(let char of str.toLowerCase()){
+//         if (vowelsArr.includes(char)) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+// MY ITERATIVE
+// function vowels(str) {
+//     let vowelsArr = ['a', 'e', 'i', 'o', 'u']
+//     let foundVowels = str.toLowerCase().split('').filter(char => vowelsArr.includes(char))
+//     return foundVowels.length
+// }
+
+// MY REGEX
+// function vowels(str) {
+//     let foundVowels = str.toLowerCase().match(/[aeiou]/g) || []
+//     return foundVowels.length
+// }
