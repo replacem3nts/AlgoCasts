c19e4250aa14256cab3e58d4aaaa3ecb106b1a64
replacem3nts <donny.landis@gmail.com>
2020-07-03 14:15:43 -0400

    solves matrix and fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,56 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    let cache = {}
+    return function(...args) {
+        if(cache[args]) {
+            return cache[args]
+        }
+        let result = fn.apply(this, args)
+        cache[args] = result
+
+        return result
+    }
+}
+
+function slowFib(n) {
+    if(n < 2) {
+        return n
+    }
+
+    return fib(n - 1) + fib(n - 2) 
+}
+
+let fib = memoize(slowFib)
 
 module.exports = fib;
+
+// HIST RECURSIVE - RUNTIME EXPONETIAL (2^N)
+// function fib(n) {
+//     if(n < 2) {
+//         return n
+//     }
+
+//     return fib(n - 1) + fib(n - 2) 
+// }
+// HIS ITERATIVE - RUNTIME IS LINEAR
+// function fib(n) {
+//     let result = [0, 1]
+
+//     for(let i = 2; i <= n; i++) {
+//         let a = result[i - 1]
+//         let b = result[i - 2]
+//         result.push(a + b)
+//     }
+
+//     return result[n]
+// }
+// MY ITERATIVE - RUNTIME IS LINEAR
+// function fib(n) {
+//     let fibArr = [0, 1]
+//     for(let i = 0; i < n; i++) {
+//         fibArr.push(fibArr[i] + fibArr[i + 1])
+//     }
+//     return fibArr[n]
+// }
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -1,4 +1,4 @@
-// --- Directions
+// --- counts
 // Write a function that accepts an integer N
 // and returns a NxN spiral matrix.
 // --- Examples
@@ -15,6 +15,42 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let spiralMatrix = []
+    for (let i = 0; i < n; i++) {
+        spiralMatrix.push([])
+    }
+    let counter = 1
+
+    let startRow = 0
+    let endRow = n - 1
+    let startColumn = 0
+    let endColumn = n - 1
+
+    while(startColumn <= endColumn && startRow <= endRow) {
+        for(let i = startColumn; i <= endColumn; i++) {
+            spiralMatrix[startRow][i] = counter
+            counter++
+        }
+        startRow++
+        for(let x = startRow; x <= endRow; x++) {
+            spiralMatrix[x][endColumn] = counter
+            counter++
+        }
+        endColumn--
+        for(let y = endColumn; y >= startColumn; y--) {
+            spiralMatrix[endRow][y] = counter
+            counter++
+        }
+        endRow--
+        for(let z = endRow; z >= startRow; z--) {
+            spiralMatrix[z][startColumn] = counter
+            counter++
+        }
+        startColumn++
+    }
+    console.log(spiralMatrix)
+    return spiralMatrix
+}
 
 module.exports = matrix;
