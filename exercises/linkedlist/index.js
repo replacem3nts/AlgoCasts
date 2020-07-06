2be977a674e4459e9b5619b5776a7301cf7ddb50
replacem3nts <donny.landis@gmail.com>
2020-07-05 20:53:05 -0400

    passing approx half linked list tests

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,101 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head)
+    }
+
+    size() {
+        let counter = 0
+        let node = this.head
+        while(node) {
+            counter++
+            node = node.next
+        }
+        return counter
+    }
+
+    getFirst() {
+        return this.head
+    }
+
+    getLast() {
+        if(!this.head) {
+            return null
+        }
+
+        let node = this.head
+        while(node) {
+            if(!node.next) {
+                return node
+            }
+            node = node.next
+        }
+        return node
+    }
+
+    clear() {
+        this.head = null
+    }
+
+    removeFirst() {
+        if(!this.head) {
+            return null
+        }
+
+        this.head = this.head.next
+    }
+
+    removeLast() {
+        if(!this.head) {
+            return null
+        }
+
+        if(!this.head.next) {
+            this.head = null
+            return 
+        }
+        
+        let previous = this.head
+        let node = this.head.next
+
+        while(node.next) {
+            previous = node
+            node = previous.next
+        }
+        previous.next = null  
+    }
+
+    insertLast(data) {
+        let last = this.getLast()
+        let newNode = new Node(data)
+        last ? last.next = newNode : this.head = newNode
+    }
+
+    getAt(index) {
+        let counter = 0
+        let node = this.head
+        while(node) {
+            if(counter === index) {
+                return node
+            }
+            counter++
+            node = node.next
+        }
+        return null
+    }
+}
 
 module.exports = { Node, LinkedList };
