c256a6ce75d81a3878285449d356bb61da5204e0
replacem3nts <donny.landis@gmail.com>
2020-07-12 17:26:20 -0400

    solves tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,47 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data
+        this.children = []
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data))
+    }
+
+    remove(data) {
+        this.children = this.children.filter(child => {
+            return child.data !== data
+        })
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null
+    }
+
+    traverseBF(fn) {
+        let travArr = [this.root]
+
+        while(travArr.length) {
+            let node = travArr.shift()
+            travArr.push(...node.children)    
+            fn(node)
+        }
+    }
+
+    traverseDF(fn) {
+        let travArr = [this.root]
+
+        while(travArr.length) {
+            let node = travArr.shift()
+            travArr.unshift(...node.children)
+            fn(node)
+        }
+    }
+}
 
 module.exports = { Tree, Node };
