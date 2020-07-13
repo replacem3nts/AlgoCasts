976f0248145e830fae86807e98db884580c1da26
replacem3nts <donny.landis@gmail.com>
2020-07-13 15:01:41 -0400

    solves bst, levelwidth, validate

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let travArr = [root, 's']
+    let widths = [0]
+    while(travArr.length > 1) {
+        let nextItem = travArr.shift()
+        if(nextItem === 's') {
+            widths.push(0)
+            travArr.push('s')
+        } else {
+            widths[widths.length - 1]++
+            travArr.push(...nextItem.children)
+        }
+    }
+    return widths
+}
 
 module.exports = levelWidth;
